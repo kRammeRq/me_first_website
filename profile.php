1fb129eb97a1078b832ccc152e1fab30eb485a9c
kRammeRq <den4ik2319@gmail.com>
2024-04-28 07:28:14 -0400

    3 lesson

--- profile.php
+++ profile.php
@@ -98,7 +98,7 @@ if (isset($_POST['submit'])) {
         if (((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg")
         || (@$_FILES["file"]["type"] == "image/jpg") || (@$_FILES["file"]["type"] == "image/pjpeg")
         || (@$_FILES["file"]["type"] == "image/x-png") || (@$_FILES["file"]["type"] == "image/png"))
-        && (@$_FILES["file"]["size"] < 102400))
+        && (@$_FILES["file"]["size"] < 10240000))
         {
             move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
             echo "Load in:  " . "upload/" . $_FILES["file"]["name"];
